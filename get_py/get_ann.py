b287e3e9e77e05fad3da2221ef8ba3c4b0c8f1d7
Ota1022 <itaru1022@gmail.com>
2023-01-29 18:12:01 +0900

    exp1

--- get_py/get_ann.py
+++ get_py/get_ann.py
@@ -68,8 +68,8 @@ def main():
     # keyword = "#ナインティナインANN -is:retweet"
     # keyword = "#霜降り明星ANN -is:ret
     keyword = "#annkw -is:retweet"
-    start_date = "2023-01-21T16:00:00.000Z"
-    end_date = "2023-01-21T18:05:00.000Z"
+    start_date = "2022-12-17T16:00:00.000Z"
+    end_date = "2022-12-17T18:05:00.000Z"
     max_results = 200
     total_tweets = 0
 
